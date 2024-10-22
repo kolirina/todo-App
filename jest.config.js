483ecc8ecd78ff7ced31956fb6b1d90feccc344e
kolirina <irinairin@list.ru>
2024-10-22 11:01:39 +0300

    feat: add tests for task input

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+};
